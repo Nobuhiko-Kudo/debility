9a4587f973819e0fb8ba6c8f2d1f36ae83224a24
“nkudo” <kudo_27nov@yahoo.co.jp>
2015-07-17 16:00:54 +0900

    rails-erdを導入

--- app/models/game_log.rb
+++ app/models/game_log.rb
@@ -1,7 +1,7 @@
 class GameLog < ActiveRecord::Base
   belongs_to :user
   belongs_to :game
-  has_only :pelmanism_result, dependent: :destroy
+  has_one :pelmanism_result, dependent: :destroy
 
   validates :user_id, presence: true
   validates :game_id, presence: true
