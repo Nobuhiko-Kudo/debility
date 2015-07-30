455154515177e10de7d22958a50152464eb8cf97
arai <ah.newwell@hotmail.co.jp>
2015-07-30 21:21:55 +0900

    user登録フォーム試作

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def create
+    user = User.create(:user_name => params[:user_name])
+    cookies.permanent['user_id'] = user.id
+    render json: user
   end
 
   def update
