db970c25a8b64efb981a05cc721a1f43618fc890
“nkudo” <kudo_27nov@yahoo.co.jp>
2015-09-20 17:39:24 +0900

    ルーレットを改善し履歴表示するようにした

--- app/assets/javascripts/angular/controllers/RouletteCtrl.coffee.js
+++ app/assets/javascripts/angular/controllers/RouletteCtrl.coffee.js
@@ -1,62 +1,64 @@
-window.app.controller('RouletteCtrl', ['$scope', function($scope){
-  $scope.click_roulette = function(){
-    //回転速度
-    var speed = 3;
-    //ルーレットの分割数
-    var divied =6;
-    //◯秒後に停止する時間
-    var timeout = 8000;
-      
-    //停止位置の設定
-    var stopAngle = Math.round(Math.random() * 360 + 0.5);
-
-    //ルーレットの角度の変数
-    var angle = stopAngle;
-
-    //ルーレットの分割数から1エリア分の角度を求める。
-    var section = 360/divied;
-
-    //停止位置がどのエリアにあるかを調べて、該当する番号をstopNumberに格納する
-    for(i=1; i<=divied; i++){
-      if(section*(i-1)+1 <= stopAngle && stopAngle <= section*i){
-        stopNumber = i
-      }
-    };
-
-    //5mmsecでspeed分の数値分回転する
-    var rotation = setInterval(function(){
-      $("#moto").rotate(angle);
-      angle += speed;
-    },5);
-
-    //timeout秒後に停止させる処理
-    setTimeout(function(){
-      //クルクル処理をしているsetIntervalをclear
-      clearInterval(rotation);
-      //setIntervalで増えた余分な数値を減らし、逆回転を防ぐためにマイナス値にする
-      angle = angle%360-360;
-      //停止位置までのアニメーション。完了するとresult()が実行される
-      $("#mato").rotate({
-        angle: angle, 
-        animateTo: stopAngle,
-        callback: result
-      });
-    }, timeout);
+window.app.controller('RouletteCtrl', ['$scope', '$timeout', function($scope, $timeout){
+  $scope.stop_roulette = true;
+  $scope.resultArray = [];
+  $scope.count = 0;
+  $scope.isResult = function(){
+    return $scope.resultArray.length != 0;
   };
+    var walking;
 
-  $scope.catwalk = (function(win, doc) {
-    "use strict";
-    var img = doc.getElementById("img"),
+  $scope.click_roulette = function(){
+    $scope.stop_roulette = false;
+    catwalk();
+    $scope.stopColor = "";
+    $scope.isWhite = false;
+    var timeout = 5800;
+    var random = Math.round(Math.random() * 360 +0.5);
+    var animate;
+    var img,
         classList = [
-            "",
-            "open"
-        ],
+                      "",
+                      "open"
+                    ],
         length = classList.length,
         index = 0;
-    
-    setInterval(function() {
+
+    function catwalk() {
+      walking = setTimeout(function walk() {
+        img = document.getElementById("img");
         img.className = classList[index];
         index = ++index % length;
-    }, 200);  
-  })(this, document);
-}]);
+        catwalk();
+      }, 500);
+    };
+       
+
+    if(random % 2 == 1){
+      animate = 3780;
+    }else{
+      animate = 3600;
+    }
+
+    $("#moto").rotate({
+      angle: 0,
+      animateTo:animate,
+      duration:timeout
+    });
+
+    $timeout(
+      function(){
+        $scope.count = $scope.count + 1;
+        $scope.stop_roulette = true;
+        clearTimeout(walking);
+        if(animate == 3780){
+          $scope.stopColor = "白";
+          $scope.isWhite = true;
+        }else if(animate == 3600){
+          $scope.stopColor = "赤";
+          $scope.isWhite = false;
+        }
+        $scope.resultArray.push([$scope.count, $scope.stopColor]);
+      }, timeout);
+   }
+  
+  }]);
