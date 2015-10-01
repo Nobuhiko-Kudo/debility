0bfb6d3163c9815c06de5ba21a7d211dd063a211
“nkudo” <kudo_27nov@yahoo.co.jp>
2015-10-02 00:20:26 +0900

    seedでgame_infoに値を入力するように変更

--- db/seeds/development/games.rb
+++ db/seeds/development/games.rb
@@ -1,12 +1,12 @@
-game_names = []
+games = []
 
-game_names.push "神経衰弱"
-game_names.push "ルーレット"
+games.push game_name: "神経衰弱", game_info: '神経衰弱の説明'
+games.push game_name:"ルーレット", game_info:'ルーレットをクリックして猫の位置に止まる色を当てるゲーム'
 
 3.times do |i|
-  game_names.push "dev_game_#{i}"
+  games.push game_name:"dev_game_#{i}", game_info:"dev_game_#{i}の説明"
 end
 
-game_names.each do |game_name|
-  Game.create(game_name: game_name) unless Game.exists? game_name: game_name
+games.each do |game|
+  Game.create(game) unless Game.exists? game
 end
