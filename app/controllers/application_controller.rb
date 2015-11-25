97526fd4b3fc5c5a4b12c910a8e297357b41af77
arai <ah.newwell@hotmail.co.jp>
2015-11-25 21:55:20 +0900

    deviceを実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,18 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :auth_user
+  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def auth_user
-    user_id = cookies['user_token']
-    if(user_id)
-      user = User.find(user_id)
-      @registed_user_name = user.user_name
-    end
+  # def auth_user
+  #   user_id = cookies['user_token']
+  #   if(user_id)
+  #     user = User.find(user_id)
+  #     @registed_user_name = user.user_name
+  #   end
+  # end
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :user_name
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,12 @@
 class UsersController < ApplicationController
   def create
-    user = User.create!(:user_name => params[:user_name])
-    cookies.permanent['user_token'] = user.id
-    #render json: { user_name:user.user_name }
-    redirect_to games_url
-  rescue
-    flash[:error] = "別の名前でおねがい"
-    redirect_to games_url
+  #   user = User.create!(:user_name => params[:user_name])
+  #   cookies.permanent['user_token'] = user.id
+  #   #render json: { user_name:user.user_name }
+  #   redirect_to games_url
+  # rescue
+  #   flash[:error] = "別の名前でおねがい"
+  #   redirect_to games_url
   end
 
   def update
